054ca2d449e40af3be94d71dc6d7f44efe047676
mb20ot <133376358+mb20ot@users.noreply.github.com>
2023-05-25 09:49:31 -0400

    create documents branch

--- timeline/src/lib/components/PageTransitionBlur.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	import { blur } from "svelte/transition";
-</script>
-
-<div
-	in:blur={{duration: 250, delay: 251 }}
-	out:blur={{ duration: 250 }}>
-	<slot />
-</div>
--- timeline/src/lib/components/PageTransitionFly.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	import { fly } from "svelte/transition";
-</script>
-
-<div
-	in:fly={{ y: -50, duration: 500, delay: 501 }}
-	out:fly={{ y: -50, duration: 500 }}>
-	<slot />
-</div>
