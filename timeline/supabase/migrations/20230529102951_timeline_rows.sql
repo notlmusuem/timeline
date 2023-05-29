7f389cba3fe8941cac5b9581ecd2bf5ac8afd933
mb20ot <133376358+mb20ot@users.noreply.github.com>
2023-05-29 18:17:53 -0400

    constraint should check nullity equality wrt to the columns, #16

--- timeline/supabase/migrations/20230529102951_timeline_rows.sql
+++ timeline/supabase/migrations/20230529102951_timeline_rows.sql
@@ -5,5 +5,8 @@ alter table public.timeline
   add column if not exists end_date date null,
   add column if not exists end_date_precision date_precision default 'day',
   alter column created_at set not null,
-  -- ensure that end_date == NULL implies end_date_precision == NULL
-  add constraint timeline_end_date_precision_null check (end_date is not null or end_date_precision is null);
+  -- ensure that (end_date == NULL) == (end_date_precision == NULL)
+  add constraint timeline_end_date_precision_null check (
+    (end_date is not null and end_date_precision is not null)
+    or (end_date is null and end_date_precision is null)
+  );
