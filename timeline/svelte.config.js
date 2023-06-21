be1640c20fad7ae5fdead39241050652a28dd1d8
mb20ot <133376358+mb20ot@users.noreply.github.com>
2023-06-21 18:50:54 -0400

    bring in svelte-preprocess-import-assets to fix relative assets breaking

--- timeline/svelte.config.js
+++ timeline/svelte.config.js
@@ -1,5 +1,6 @@
 import adapter from '@sveltejs/adapter-node';
 
+import { importAssets } from 'svelte-preprocess-import-assets';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -7,6 +8,12 @@ export default {
   kit: {
     adapter: adapter(),
   },
-  preprocess: vitePreprocess(),
+  files: {
+    serviceWorker: "src/service-worker"
+  },
+  preprocess: [importAssets(), vitePreprocess()],
   prerender: true,
+  serviceWorker: {
+    register: true
+  }
 };
