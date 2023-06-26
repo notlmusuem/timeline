44541afc0124fbe920fd37c37468e2a32c41952b
mb20ot <133376358+mb20ot@users.noreply.github.com>
2023-06-26 14:36:15 -0400

    rm old route

--- timeline/src/routes/timeline/+page.server.js
+++
@@ -1,9 +0,0 @@
-import supabase from "$lib/supabaseClient";
-import { Entry } from "$lib/models/timeline";
-
-export async function load() {
-  return {
-    // svelte's load does not support serializing non-json objects (why?)
-    timeline: (await Entry.select_all(supabase)).map(e => e.to_obj())
-  };
-}
