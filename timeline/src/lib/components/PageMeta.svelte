0c37bf8730c8cd7f03578be63a1a312b0a123460
mb20ot <133376358+mb20ot@users.noreply.github.com>
2023-06-27 13:47:15 -0400

    create pagemeta component for #32

---
+++ timeline/src/lib/components/PageMeta.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+
+  export let titles: string[];
+  export let site_name: string;
+  export let description: string|null = null;
+  export let image: string|null = null;
+</script>
+
+
+<title>{titles.join(" | ")} | {site_name}</title>
+<meta property="og:title" content={titles.join(" | ")}>
+<meta property="og:site_name" content={site_name}>
+<meta property="og:locale" content="en_CA">
+<meta property="og:type" content="website">
+
+<!-- todo: add alternate locales for issue #15 -->
+<!-- todo: also set <html lang={lang}> for that -->
+<!-- <meta property="og:locale:alternate" content="fr_CA"> -->
+
+{#if description != null}
+  <meta property="og:description" content={description}>
+  <meta name="description" content={description}>
+{/if}
+
+{#if $page.url != null}<meta property="og:url" content="{$page.url.toString()}">{/if}
+{#if image != null}<meta property="og:image" content="{image}">{/if}
