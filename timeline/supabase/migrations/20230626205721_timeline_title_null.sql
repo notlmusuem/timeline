786bd1e01558981471c52f124971384a9c15626d
mb20ot <133376358+mb20ot@users.noreply.github.com>
2023-06-27 13:33:34 -0400

    fix the date_date_precision default while we're at it

--- timeline/supabase/migrations/20230626205721_timeline_title_null.sql
+++ timeline/supabase/migrations/20230626205721_timeline_title_null.sql
@@ -1,4 +1,5 @@
 -- remove nullibility from title and set it's default value appropriately
 alter table public.timeline
   alter column title set default '',
-  alter column title set not null;
+  alter column title set not null,
+  alter end_date_precision set default null;
