f704b2a6543ecfed478a26c22c935e12a2b6b02c
mb20ot <133376358+mb20ot@users.noreply.github.com>
2023-07-14 18:37:59 -0400

    load event, loading hotswaps the datauri, add a loader icon

--- timeline/src/lib/components/QrEntry.svelte
+++ timeline/src/lib/components/QrEntry.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
   import { browser } from "$app/environment";
-  import { QRCodeCanvas as QrCode, type DotType, type Options } from "@loskir/styled-qr-code-node";
+  import { createEventDispatcher } from "svelte";
+  import { type QRCodeCanvas as QrCode } from "@loskir/styled-qr-code-node";
 
   import { Entry } from "$lib/models/timeline";
   import { qr_from_entry } from "$lib/qr";
   import { buffer_download, buffer_to_datauri } from "$lib/utils";
+
+
+  const dispatch = createEventDispatcher();
 
   export let entry: Entry;
   export let size: number;
@@ -12,29 +16,61 @@
   export let ssr: boolean = false;
 
   let qr: QrCode;
+  let datauri: string|null = null;
+
+  $: {
+    entry;
+    color;
+    datauri = null;
+  }
 
   $: if (browser || ssr) {
     qr = qr_from_entry(entry, {
       size: size, color: color
     });
+
+    (async () => {
+      datauri = buffer_to_datauri(
+        await qr.toBuffer("png"), "image/png"
+      );
+    })();
   }
 
-  export async function download() {
-    buffer_download(await qr.toBuffer(), "qr.png", "image/png");
+  export async function download(size: number|null = null) {
+    const print_qr = qr_from_entry(entry, {
+      size: size ?? $$props.size, color: color
+    });
+    buffer_download(await print_qr.toBuffer(), "qr.png", "image/png");
   }
 </script>
 
-{#await qr.toBuffer("png")}
-  <div style={`height: ${size}px; width: ${size}px;`}></div>
-{:then buffer}
-  <img src={buffer_to_datauri(buffer, "image/png")} height={size} width={size}
-  alt="A QR code" />
-{/await}
+
+{#if datauri != null}
+  <img
+    src={datauri} height={size} width={size}
+    style="width: 100%; height: auto; aspect-ratio: 1;"
+    on:load={async () => {
+      dispatch("load");
+    }}
+    alt="A QR code" />
+{:else}
+  <div style="padding: 0 auto;">
+    <i class="material-symbols-rounded loader">progress_activity</i>
+  </div>
+{/if}
+
 
 <style>
-  img {
-    width: 100%;
-    height: auto;
-    aspect-ratio: 1;
+  .loader {
+    animation: spin 1s linear infinite;
+  }
+
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
   }
 </style>
