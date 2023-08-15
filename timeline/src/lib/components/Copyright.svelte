becb73b9682af400974e1efbfaf28353914a7c50
mb20ot <133376358+mb20ot@users.noreply.github.com>
2023-08-15 12:54:13 -0400

    update copyright due to our aug 15th meeting

--- timeline/src/lib/components/Copyright.svelte
+++ timeline/src/lib/components/Copyright.svelte
@@ -1,20 +1,51 @@
 <script lang="ts">
+  import OverlayPopup from "./OverlayPopup.svelte";
+
   export let short: boolean = false;
+
+  let popup: boolean = false;
 </script>
 
-{#if short}
-  &copy; 2023, by the project contributors <a href="/about#about-this-timeline">Alec Ames et al.</a>
-  Freely licensed under <a href="https://github.com/notlmusuem/timeline">MIT</a>.
-  <br>Funded in part by ... and Brock University through the ... grant.
-{:else}
-  &copy; 2023, by the project contributors <span class='nbr'>Alec Ames</span>, <span class='nbr'>Ibrahim Hashmi</span>, <span class='nbr'>Francis Monwe</span>, <span class='nbr'>Matthew Benson</span>, <span class='nbr'>Tommy Pham</span>, <span class='nbr'>Abhijeet Prajapati</span>, <span class='nbr'>Justin Stickel</span>, <span class='nbr'>Haaris Yahya</span>, and under the supervision of <span class='nbr'>Naser Ezzati-Jivan</span>, by <span class='nbr'>Muhammed Bilal</span> and <span class='nbr'>Michael Boulet</span>.
-  Freely licensed under <a href="https://github.com/notlmusuem/timeline">MIT</a>.
-  Funded in part by ... and Brock University through the ... grant.
-{/if}
+<svelte:window on:click={() => { popup = false; }} />
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<div>
+  {#if short}
+    <span>
+      &copy; 2023, by the project contributors.
+      Freely licensed under <a href="https://github.com/notlmusuem/timeline">MIT</a>.
+      <br>Funded in part by ... and Brock University through the VPMI/FedDev Grant.
+    </span>
+  {:else}
+    <span style='display: inline-block;'>&copy; 2023, by the</span>
+    <span style='display: inline-block;' on:click|stopPropagation>
+      {#if popup}
+        <OverlayPopup direction="top" offset={0.15} fix_arrow>
+          <div class="popup">
+            <span class="nbr">Alec Ames</span>, <span class="nbr">Ibrahim Hashmi</span>, <span class="nbr">Francis Monwe</span>, <span class="nbr">Matthew Benson</span>, <span class="nbr">Tommy Pham</span>, <span class="nbr">Abhijeet Prajapati</span>, <span class="nbr">Justin Stickel</span>, <span class="nbr">Haaris Yahya</span>, <span class="nbr">Muhammed Bilal</span> and <span class="nbr">Michael Boulet</span>.
+          </div>
+        </OverlayPopup>
+      {/if}
+      <a on:click|preventDefault={() => { popup = !popup; }}>project contributors</a>.
+    </span>
+    <span>
+      Freely licensed under <a href="https://github.com/notlmusuem/timeline">MIT</a>.
+      Funded in part by ... and Brock University through the VPMI/FedDev Grant.
+    </span>
+  {/if}
+</div>
 
 <style>
   .nbr {
     word-break: keep-all;
     white-space: nowrap;
+  }
+
+  .popup {
+    max-width: min(40em, 70vw);
+    width: max-content;
+    padding: 1rem;
+    z-index: -99;
+    font-size: var(--font-size-smallish)
   }
 </style>
